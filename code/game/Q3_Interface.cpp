c53d86df8b938787af851d7352c728f16ce793c4
kugelrund <marcel.jacobse@ewetel.net>
2018-04-07 17:11:03 +0200

    Print speedrun time on death of final boss
    Pause speedrun timer and print level and total time when final boss is
    defeated, to get final time of a full game run or level time for kor2
    ILs. Handles both light and dark side. Using the load of the
    skip-scripts for the final cutscenes as trigger.

--- code/game/Q3_Interface.cpp
+++ code/game/Q3_Interface.cpp
@@ -4787,6 +4787,12 @@ static void Q3_SetCinematicSkipScript( char *scriptname )
 	}
 	else
 	{
+		if (strcmp(scriptname, "kor2/endgame_ds_3_skip") == 0 ||
+		    strcmp(scriptname, "kor2/endgame_ls_4_skip") == 0)
+		{
+			cgi_SpeedrunPauseTimer();
+			cgi_SpeedrunLevelFinished();
+		}
 		Q_strncpyz(cinematicSkipScript,scriptname,sizeof(cinematicSkipScript));
 	}
 
